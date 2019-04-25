906e61d4e5129e6adfb6996e002f1e4f48e79f5b
matrayu <northridge614@gmail.com>
2019-04-25 07:05:17 -0700

    Delete BookmarkContext.js

--- src/BookmarkContext.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const BookmarkContext = React.createContext({
-    bookmarks: [],
-    addBookmark: () => {},
-    deleteBookmarks: () => {},
-})
-
-export default BookmarkContext
