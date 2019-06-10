776151868cc12cb914ed00a42f90ee1a2012dccf
matrayu <northridge614@gmail.com>
2019-06-10 08:37:27 -0700

    Update to add specific endpoint

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  API_ENDPOINT: `https://enigmatic-depths-44932.herokuapp.com/`,
+  API_ENDPOINT: `https://enigmatic-depths-44932.herokuapp.com/api/bookmarks`,
   //API_ENDPOINT: `http://localhost:8000/api/bookmarks`,
   API_TOKEN: process.env.REACT_APP_API_TOKEN,
 }
